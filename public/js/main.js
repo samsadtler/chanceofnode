ed76197fd69f1435d166a7ea99ae70317b4d9584
Sam Sadtler <sam.sadtler@gmail.com>
2016-04-25 22:39:24 -0400

    sex on a beach

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,13 @@
-// CUSTOM JS FILE //
+
+function suncalcRequest(){
+	jQuery.ajax({
+			url : '/api/get',
+			dataType : 'json',
+			success : function(response) {
+				console.log("calculation response", response);
+				}
+				console.log("calculation");
+				// now, render the animal image/data
+
+	})
+}
