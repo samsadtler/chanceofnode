6d403cea78b4fd447bd93e8a76880dfc2d098d39
Sam Sadtler <sam.sadtler@gmail.com>
2016-04-30 17:06:06 -0400

    adding github path

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,4 @@
 var localhost = "https://chanceofrainbows.herokuapp.com";
-// var localhost = "https://localhost:3000";
-
 function init(){
 	console.log("init babies")
 	// var date = new Date('2013-03-05UTC'),
